8662237203692f8976f8138be15a002aa0dab251
aayush920 <76609501+aayush920@users.noreply.github.com>
2021-10-02 21:29:45 +0530

    Create 1.cpp

---
+++ cf contest/745/1.cpp
@@ -0,0 +1,122 @@
+#include<bits/stdc++.h>
+using namespace std;
+//common file for PBDS
+#include<ext/pb_ds/assoc_container.hpp>
+//including tree_order_statistics_node_update
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+//macro definition
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+#define ll long long
+#define lld long double
+#define ull unsigned long long
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define repk(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? i++ : i--)
+#define bit(x) __builtin_popcount(x)
+#define bitll(x) __builtin_popcountll(x)
+#define popb pop_back
+#define pb push_back
+#define eb emplace_back
+#define ub upper_bound
+#define lb lower_bound
+#define ff first
+#define ss second
+#define um unordered_map
+#define om ordered_map
+#define uniq(x) (x).erase(unique(all(x)), (x).end())
+#define precision(x, y) fixed << setprecision(y) << x
+#define PI 3.1415926535897932384626
+#define sz(x) ((ll)(x).size())
+#define present(b, a) ((a).find((b)) != (a).end())  //if b is present in a
+const ll mod = 1e9 + 7; //1000000007
+const ll mod2 = 998244353;
+const ll inf = LLONG_MAX;
+const lld epsilon = 1e-12;
+typedef pair<ll, ll> pl;
+typedef pair<int, int> pi;
+typedef pair<lld, lld> pd;
+typedef vector<ll> vl;
+typedef vector<pl> vpl;
+typedef vector<vl> vvl;
+typedef vector<int> vi;
+typedef vector<pi> vpi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef vector<vector<string>> vvs;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+// ------------------------Do not write above this line-----------------------------------------------------------------------------------------------------------------
+ll power(ll a,ll b,ll mod){
+	a=a%mod;
+	ll res=1;
+	while(b){
+		if(b&1){
+			res=(res*a)%mod;
+		}
+        a=(a*a)%mod;
+		b=b/2;
+	}
+	return res%mod;
+}
+
+ll inverse(ll a,ll b){
+    return power(a,b-2,b);
+}
+
+void solve(){
+    ll n;
+    cin>>n;
+    ll fact[2*n+1]={0};
+    fact[0]=1;
+    for(int i=1;i<=2*n;i++){
+        fact[i]=(i*fact[i-1])%mod;
+    }
+    ll ans=(fact[2*n-1])%mod;
+    ans=(ans%mod*n%mod)%mod;
+    cout<<ans<<endl;
+}
+
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ll t;
+    t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
